44ac98b81ab85ede6015ebc5139a5a391e164269
troy <troytran000@gmail.com>
2025-08-29 21:18:41 -0500

    static hosting

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ dotenv.config()
 
 const app = express();
 
+const PORT = process.env.PORT
+
+app.use(express.static("public"));
+
+app.listen(PORT, () =>{
+  console.log(`Listening at port ${PORT}`)
+})
 
 
